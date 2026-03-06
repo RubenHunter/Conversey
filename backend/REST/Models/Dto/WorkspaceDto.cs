b0505d3905419b009718f37c7bee382741743c33
Nowipi <noah.uyttebroeck@gmail.com>
2026-03-06 13:39:09 +0100

    Basic workspace endpoint.

---
+++ backend/REST/Models/Dto/WorkspaceDto.cs
@@ -0,0 +1,17 @@
+﻿using Conversey.BL.Domain.Entities.Identity;
+
+namespace Conversey.REST.Models.Dto;
+
+public class WorkspaceDto
+{
+    public string Name { get; set; }
+
+
+    public static WorkspaceDto From(Workspace workspace)
+    {
+        return new WorkspaceDto
+        {
+            Name = workspace.Name,
+        };
+    }
+}
--- backend/REST/Program.cs
+++ backend/REST/Program.cs
@@ -1,10 +1,13 @@
+using Conversey.BL;
+using Conversey.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<IRepository, InMemoryRepository>();
+builder.Services.AddScoped<IManager, Manager>();
 
 var app = builder.Build();
 
