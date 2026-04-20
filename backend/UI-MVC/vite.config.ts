c76494ebaa5eef2276927c069d4aa56e1893d71b
Nowipi <noah.uyttebroeck@gmail.com>
2026-04-20 17:07:09 +0200

    The rest

---
+++ backend/UI-MVC/vite.config.ts
@@ -0,0 +1,41 @@
+import { UserConfig, defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
+
+
+export default defineConfig(async () => {
+    const files = fs.readdirSync('./Assets');
+    const inputEntries = files
+			.filter(file => file.endsWith('.ts') )
+        .reduce((acc, file) => {
+            const fileName = path.parse(file).name;
+            acc[fileName] = path.join('./Assets', file);
+            return acc;
+        }, {} as Record<string, string>);
+
+    const config: UserConfig = {
+        appType: 'custom',
+        root: 'Assets',
+        publicDir: 'public',
+			plugins: [
+				tailwindcss(),
+			],
+        build: {
+            emptyOutDir: true,
+            manifest: true,
+            outDir: '../wwwroot',
+            assetsDir: '',
+            rollupOptions: {
+                input: inputEntries
+            },
+        },
+        server: {
+            strictPort: true,
+        },
+        optimizeDeps: {
+            include: []
+        }
+    }
+    return config;
+});
