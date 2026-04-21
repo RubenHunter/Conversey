cd2d410f7ab20fc5dbc0c6df6ceec0fd6dd8b2cc
ruben-kdg <ruben.moons.1@student.kdg.be>
2026-04-21 14:16:08 +0200

    fixed errors and added consistent button styling. Removed old mvc folder

--- backend/UI-MVC-old/Models/Dto/CreateResponseReactionRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Conversey.UI_MVC.Models.Dto;
-
-public class CreateResponseReactionRequestDto
-{
-    public string Emoji { get; set; }
-    public Guid YouthId { get; set; }
-}
-
--- backend/UI-MVC-old/Models/Dto/CreateResponseRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Conversey.UI_MVC.Models.Dto;
-
-public class CreateResponseRequestDto
-{
-    public string Text { get; set; }
-    public Guid YouthId { get; set; }
-}
-
